b859e031774374428237b3c6cd61b0a232f55f8e
mcc <mcc@mcc.mcc>
2026-07-13 19:28:48 +0800

    增加邮箱验证登录和管理员教程

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 window.QUESTION_BOX_CONFIG = {
   VITE_API_MODE: "supabase",
   VITE_SUPABASE_URL: "https://qjuawlsxgvycxsuahfeg.supabase.co",
-  VITE_SUPABASE_ANON_KEY: "sb_publishable_wa77r3qM7PiveglwnCgOgg_J6a9I3Qe"
+  VITE_SUPABASE_ANON_KEY: "sb_publishable_wa77r3qM7PiveglwnCgOgg_J6a9I3Qe",
+  VITE_ADMIN_USER_ID: "f8b68abd-d224-401e-a539-b475e904d189",
+  VITE_ADMIN_REMARK: "使用教程"
 };
